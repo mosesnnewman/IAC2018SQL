a052045d1c6741f92d322351695a569a5904e172
mosesnnewman <newman72@sbcglobal.net>
2020-09-14 10:15:41 -0400

    Fix login for security code on sms message form. Remove solid brush declaration in colortextbox.

--- IAC2018SQL/ColorTextBox.cs
+++ IAC2018SQL/ColorTextBox.cs
@@ -29,7 +29,7 @@ namespace IAC2018SQL
         protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
         {
             base.OnPaint(e);
-            System.Drawing.SolidBrush textBrush;
+            //System.Drawing.SolidBrush textBrush;
 
             if (!Enabled)
             {
@@ -42,13 +42,13 @@ namespace IAC2018SQL
                 _foreColorCustom = Color.Red;
             else
                 if (this.Text.IndexOf('$') >= 0 && this.Text.Trim() != "$0.00")
-                _foreColorCustom = Color.Green;
+                    _foreColorCustom = Color.Green;
             else
-                    if (!Enabled)
-                _foreColorCustom = System.Drawing.SystemColors.GrayText;
-            else
-                _foreColorCustom = System.Drawing.SystemColors.ControlText;
-            textBrush = new System.Drawing.SolidBrush(_foreColorCustom);
+                if (!Enabled)
+                    _foreColorCustom = System.Drawing.SystemColors.GrayText;
+                else
+                    _foreColorCustom = System.Drawing.SystemColors.ControlText;
+            // textBrush = new System.Drawing.SolidBrush(_foreColorCustom);
             // Moses Newman 09/05/2020 GDI Drawtext Renders Text MUCH better thatn Graphics.DrawString!
             /*StringFormat sf = new StringFormat();
             sf.LineAlignment = StringAlignment.Far;
@@ -57,7 +57,7 @@ namespace IAC2018SQL
             TextFormatFlags flags = TextFormatFlags.Right | TextFormatFlags.NoPadding;
             //Paint text
             //e.Graphics.DrawString(Text,Font, textBrush, ClientRectangle,sf);
-            TextRenderer.DrawText(e.Graphics,Text.Trim(), Font, ClientRectangle, _foreColorCustom,flags);
+            TextRenderer.DrawText(e.Graphics,Text, Font, ClientRectangle, _foreColorCustom,flags);
         }
     }
 }
